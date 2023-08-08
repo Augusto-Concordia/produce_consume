a6ee2e3e75370fbbfceb7f511c50ca3f806875ad
AmritSohpal <63606966+AmritSohpal@users.noreply.github.com>
2023-08-08 13:29:39 -0400

    minor changes
    just added a scanner and the handling of q ( %15 )

--- src/Main.java
+++ src/Main.java
@@ -1,8 +1,40 @@
 import java.util.ArrayList;
-
+import java.util.Scanner;
 public class Main {
     public static void main(String[] args) {
         final int COUNT = 10;
+
+
+        System.out.println("Enter a value for q");
+        Scanner in = new Scanner(System.in);
+        float q = -1, p = 0; //Initialization of q and p
+
+        //The handling of q
+        try{
+            q = in.nextFloat();
+            p = 1 - q;
+        }catch(Exception e){
+            System.out.println("q is NAN");
+        }
+
+
+        if (q >=0 && q <=1){
+            Producer producer = new Producer(q);
+            Consumer consumer = new Consumer(p);
+        }
+        else{
+            System.out.println("Error, q has to be less than 1 and greater than 0");
+        }
+
+
+
+
+        /*Output on the console the following:
+        Generation of an items
+        Consumption of an Item
+        isEmpty() or isFull
+        State of the processes whether they are in the state of busy waiting
+         */
 
         Semaphore mutex = new Semaphore(1);
         Semaphore full = new Semaphore(0);
@@ -10,7 +42,8 @@ public class Main {
 
         ArrayList<Integer> buffer = new ArrayList<>(COUNT);
 
-        Producer producer = new Producer(0.5f);
-        Consumer consumer = new Consumer(0.5f);
     }
+
+
+
 }
